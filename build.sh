4ea48d8159c1a0a4d10a0636fd492f168fbac046
Sander <sanderwassenberg@gmail.com>
2024-06-26 21:15:45 +0200

    linux build script

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-# go build -C -o ../run-server ./main
-
-echo "The script you are running has:"
-echo "basename: [$(basename "$0")]"
-echo "dirname : [$(dirname "$0")]"
-echo "pwd     : [$(pwd)]"
-
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo $THIS_DIR
+# go build -o ../run-server ./main
+go build -C $THIS_DIR/src -o ../run-server ./main
