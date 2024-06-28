7d817a25736d8d547c402d85bc4642881f054c93
Sander <>
2024-06-28 11:05:49 +0200

    allow origin

--- src/main/chat.go
+++ src/main/chat.go
@@ -18,6 +18,10 @@ import (
 var upgrader = ws.Upgrader{
 	// ReadBufferSize:  1024,
 	// WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+	    origin := r.Header.Get("Origin")
+	    return origin == "https://sandershowcase.hbo-ict.org"
+	}
 }
 
 type Chatrooms struct {
